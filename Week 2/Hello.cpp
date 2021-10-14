abb4263564483ee6ad116d2d84acf85957ca0114
vanloc1808 <vanloc1808@gmail.com>
2021-10-15 06:45:38 +0700

    Third of week 2

--- Week 2/Hello.cpp
+++ Week 2/Hello.cpp
@@ -4,6 +4,12 @@ int main() {
     std::cout << "Hello World!\n";
     std::cout << "Good morning!\n";
     std::cout << "Ohayou gozaimasu\n";
+    std::cout << "Konnichi wa!\n";
+    std::cout << "Komban wa!\n";
+    std::cout << "Ii tenki desu ne\n";
+    std::cout << "Samui desu ne\n";
+    std::cout << "o-genki desu ka\n";
+    std::cout << "Arigatou gozaimsu\n";
 
     return 0;
 }
