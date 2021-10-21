2d154a10937d6d3957c47e25df630024567f0504
vanloc1808 <vanloc1808@gmail.com>
2021-10-21 18:04:24 +0700

    First of week 3

---
+++ Week 3/JapaneseWeek2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "uma: horse\n";
+    cout << "mimi: ear\n";
+    cout << "mune: chest\n";
+    cout << "me: eye\n";
+    cout << "kimono: kimono\n";
+}
